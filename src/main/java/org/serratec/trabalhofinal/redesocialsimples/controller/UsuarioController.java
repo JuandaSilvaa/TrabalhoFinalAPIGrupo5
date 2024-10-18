209d4be2692aa522b0f56e7ee87b28201b251125
JuandaSilvaa <almeidajuan529@gmail.com>
2024-10-18 16:58:57 -0300

    acerto get listar

--- src/main/java/org/serratec/trabalhofinal/redesocialsimples/controller/UsuarioController.java
+++ src/main/java/org/serratec/trabalhofinal/redesocialsimples/controller/UsuarioController.java
@@ -15,6 +15,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,7 +41,9 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping
-	public ResponseEntity<List<UsuarioDTO>> listar() {
+	public ResponseEntity<List<UsuarioDTO>> listar() { 
+		UserDetails details = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		System.out.println("Login do usuario: " + details.getUsername());
 		return ResponseEntity.ok(usuarioService.findAll());
 	}
 	
